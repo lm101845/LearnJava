f4cc5883d33aa408718240a1da3a7b0e3cdcd61e
lm101845 <1018457683@qq.com>
2022-01-03 18:33:01 +0800

    Auto commit.

---
+++ 04-马士兵JavaSE/basic-code/container/src/demo01/Name.java
@@ -0,0 +1,27 @@
+package demo01;
+
+/**
+ * @Author 李明
+ * @Date 2022/1/3:17:17
+ **/
+public class Name {
+    private String firstName,lastName;
+
+    public Name(String firstName,String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
