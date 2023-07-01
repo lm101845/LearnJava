94f6341045885ea6fadbd445cd958c231b955ea8
lm101845 <1018457683@qq.com>
2023-07-01 23:36:19 +0800

    第11-15集看完

---
+++ 09-JVM/01-宋红康/02-code/JVMDemo/chapter01/src/com/atguigu/java/StackStruTest.java
@@ -0,0 +1,23 @@
+package com.atguigu.java;
+
+/**
+ * @Author liming
+ * @Date 2023/7/1 23:03
+ **/
+public class StackStruTest {
+    public static void main(String[] args) {
+        //int i = 2 + 3;
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("hello");
+    }
+}
